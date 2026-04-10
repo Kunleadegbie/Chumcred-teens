f5adf24baf446b22134d80dca00b71d43ff56568
Adekunle Adegbie <kadegbie@gmail.com>
2026-04-10 16:20:54 +0100

    fix one file

--- pages/10_Block_Unblock_Users.py
+++ pages/10_Block_Unblock_Users.py
@@ -8,8 +8,6 @@ from core.block_access import (
     parse_date,
 )
 
-role = st.session_state.get("user", {}).get("role", "student")
-render_sidebar(role)
 
 st.set_page_config(
     page_title="Block / Unblock Users",
